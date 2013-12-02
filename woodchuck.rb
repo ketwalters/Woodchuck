0dcdbd0c579a0bdb430776c768daeca211778072
Keturah Walters <your.ketwalters@gmail.com>
2013-12-01 23:42:53 -0500

    add count to file

--- woodchuck.rb
+++ woodchuck.rb
@@ -31,39 +31,40 @@ end
 rand(20).times do
 	Woodchuck.new
 end
-
+count = 0
 Woodchuck::WOODCHUCKS.each do |woodchuck|
 	rand(20).times do
 		woodchuck.chuck_wood
 	end
+	count += woodchuck.chuck_wood
 end		
 
 
+=begin
+my_woodchuck = Woodchuck.new
 
-#my_woodchuck = Woodchuck.new
+puts my_woodchuck.chuck_count
 
-#puts my_woodchuck.chuck_count
+puts "what is self INSTANCE METHOD"
 
-#puts "what is self INSTANCE METHOD"
+puts my_woodchuck.what_is_self
 
-#puts woodchuck.what_is_self
+puts "whats is self's class INSTANCE METHOD"
+puts my_woodchuck.what_is_self.class
 
-#puts "whats is self's class INSTANCE METHOD"
-#puts my_woodchuck.what_is_self.class
+puts "What is self CLASS METHOD"
+puts Woodchuck.what_is_self
 
-#puts "What is self CLASS METHOD"
-#puts Woodchuck.what_is_self
+puts "What is self's class CLASS METHOD"
+puts Woodchuck.what_is_self.class
 
-#puts "What is self's class CLASS METHOD"
-#puts Woodchuck.what_is_self.class
+my_woodchuck1 = Woodchuck.new
+my_woodchuck2 = Woodchuck.new
+my_woodchuck3 = Woodchuck.new
 
-#my_woodchuck1 = Woodchuck.new
-#my_woodchuck2 = Woodchuck.new
-#my_woodchuck3 = Woodchuck.new
-
-#Woodchuck::WOODCHUCKS.inspect
-#Woodchuck::WOODCHUCK << "woodchuck"
-#Woodchuck::WOODCHUCKS.inspect
+Woodchuck::WOODCHUCKS.inspect
+Woodchuck::WOODCHUCKS << "woodchuck"
+Woodchuck::WOODCHUCKS.inspect
 
 #Woodchuck::WOODCHUCKS
 
@@ -75,4 +76,5 @@ end
 
 #if you are in instance method then self is referring to the instance of the class
 #every other time it is referring to the class 
+=end
 
